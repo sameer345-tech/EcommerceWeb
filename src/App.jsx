9b7c0a992ef1ed2c02cbd347a327cc15bae21b49
Sameer Ahmed <sameer.3for4@gmail.com>
2025-01-15 02:52:34 -0800

    bug fixed in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-
-import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import dataBaseService from './appWrite/dataBaseService'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { fetchCardItems } from './feautures/productSlice'
-import { useQuery } from '@tanstack/react-query'
-import userAuth from './appWrite/UserAuth'
-import { login } from './store/loginReducer'
 
 function App() {
 
